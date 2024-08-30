e633af3fd6e0e38d01a2d285d60d60267bd84ae9
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-08-30 15:34:11 +0900

    ✨ Initial implementation of CarouselButton

--- src/CarouselButton.tsx
+++ src/CarouselButton.tsx
@@ -1,3 +1,7 @@
-const CarouselButton = () => <button />;
+import { ComponentPropsWithRef } from "react";
+
+const CarouselButton = (props: ComponentPropsWithRef<"button">) => (
+  <button {...props} />
+);
 
 export default CarouselButton;
