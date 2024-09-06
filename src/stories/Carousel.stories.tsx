9772dbcc01a8af0e794840c7d93d80f9f45442c1
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-09-06 17:13:01 +0900

    :wrench: Add workflow for GitHub Pages

--- src/stories/Carousel.stories.tsx
+++ src/stories/Carousel.stories.tsx
@@ -1,10 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Carousel from "../Carousel";
 import slides from "../examples/slides";
+import { fn } from "@storybook/test";
+import { useArgs } from "@storybook/preview-api";
 
 const meta = {
   title: "Example/Carousel",
   component: Carousel,
+  args: { onSlideIndexChange: fn() },
 } satisfies Meta<typeof Carousel>;
 
 export default meta;
@@ -13,5 +16,19 @@ type Story = StoryObj<typeof Carousel>;
 export const Basic: Story = {
   args: {
     slides,
+  },
+  render: function RenderCarousel(args) {
+    const [, updateArgs] = useArgs();
+
+    return (
+      <Carousel
+        {...args}
+        onSlideIndexChange={(newSlideIndex) => {
+          updateArgs({ slideIndex: newSlideIndex });
+
+          args.onSlideIndexChange?.(newSlideIndex);
+        }}
+      />
+    );
   },
 };
